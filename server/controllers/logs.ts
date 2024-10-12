ae21174c6e4f8f33df65decfb7443f79fa2bd975
yves848 <yves.godart@gmail.com>
2024-10-12 09:41:02 +0200

    WIP

--- server/controllers/logs.ts
+++ server/controllers/logs.ts
@@ -1,19 +1,19 @@
 import Logs from '../models/logs.ts';
-import {Request,Response} from 'npm:express';
+import { Request, Response } from 'npm:express';
 
-const createLog= async (req : Request ,res: Response) => {
+const createLog = async (req: Request, res: Response) => {
   console.log(req.body);
   const log = new Logs({
     date: req.body.date,
     time: req.body.time,
     status: req.body.status,
   });
-try {
-  const savedLog = await log.save();
-  res.status(201).json(savedLog);
-} catch (_error) {
-  res.status(400).json({message: _error.message});
-}
+  try {
+    const savedLog = await log.save();
+    res.status(201).json(savedLog);
+  } catch (_error) {
+    res.status(400).json({ message: _error.message });
+  }
 };
 
 export { createLog }
