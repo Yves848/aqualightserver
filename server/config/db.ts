b4f03210601f79dfaf2334900835ba66c18fac35
yves848 <yves.godart@gmail.com>
2024-10-12 10:26:10 +0200

    compilation ok

--- server/config/db.ts
+++ server/config/db.ts
@@ -2,10 +2,6 @@ import mongoose from 'npm:mongoose';
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const env = config();
-mongoose.connect(env.MONGO_URL);
-
-const db = mongoose.connection;
-
-db.on("error", (error) => console.error("Error in MongoDb connection: ", error));
-
-db.once("open", () => { console.log("Connected to MongoDB!") });
+mongoose.connect(env.MONGO_URL)
+.then(()=>{console.log('connected to MongoDB Aqualight')})
+.catch((err)=>{console.log(err)});
